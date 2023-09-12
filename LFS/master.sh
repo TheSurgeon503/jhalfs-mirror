200cc0151e7be09cd85ad2ae9b06f2f1e619a763
Pierre Labastie <pierre.labastie@neuf.fr>
2023-09-12 22:58:55 +0200

    All the targets after mk_SUDO depend on "devices"
    Now that devices.sh can be run even if the fs are already mounted
    all the targets after mounting the virtual kernel fs can depend
    on "devices". This way, if for some reason a partial build is
    restarted after a reboot, the virtual kernel fs are automatically
    re-mounted.
    
    Fixes #1737

--- LFS/master.sh
+++ LFS/master.sh
@@ -368,24 +368,24 @@ mk_SUDO: mk_LUSER
 	@sudo make BREAKPOINT=\$(BREAKPOINT) SUDO
 	@touch \$@
 
-mk_CHROOT: mk_SUDO
+mk_CHROOT: mk_SUDO devices
 	@\$(call echo_CHROOT_request)
 	@( sudo \$(CHROOT1) -c "cd \$(SCRIPT_ROOT) && make BREAKPOINT=\$(BREAKPOINT) CHROOT")
 	@touch \$@
 
-mk_BOOT: mk_CHROOT
+mk_BOOT: mk_CHROOT devices
 	@\$(call echo_CHROOT_request)
 	@( sudo \$(CHROOT1) -c "cd \$(SCRIPT_ROOT) && make BREAKPOINT=\$(BREAKPOINT) BOOT")
 	@touch \$@
 
-mk_BLFS_TOOL: create-sbu_du-report
+mk_BLFS_TOOL: create-sbu_du-report devices
 	@if [ "\$(ADD_BLFS_TOOLS)" = "y" ]; then \\
 	  \$(call sh_echo_PHASE,Building BLFS_TOOL); \\
 	  (sudo \$(CHROOT1) -c "make -C $BLFS_ROOT/work"); \\
 	fi;
 	@touch \$@
 
-mk_CUSTOM_TOOLS: mk_BLFS_TOOL
+mk_CUSTOM_TOOLS: mk_BLFS_TOOL devices
 	@if [ "\$(ADD_CUSTOM_TOOLS)" = "y" ]; then \\
 	  \$(call sh_echo_PHASE,Building CUSTOM_TOOLS); \\
 	  sudo mkdir -p ${BUILDDIR}${TRACKING_DIR}; \\
